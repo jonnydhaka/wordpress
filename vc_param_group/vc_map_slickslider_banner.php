0f1eeacc31c56068bbebff7f9cff6ea42118cdfa
Tanvir Hasan <tanvir@smartdatasoft.net>
2018-04-02 10:04:03 +0600

    file change again

--- vc_param_group/vc_map_slickslider_banner.php
+++ vc_param_group/vc_map_slickslider_banner.php
@@ -1,13 +1,10 @@
 <?php
-$fonts_array=bestel_add_fonts_family(1);
-$fonts_array2=bestel_add_fonts_family(2);
-$fonts_array3=bestel_add_fonts_family(3);
-$fonts_array4=bestel_add_fonts_family(4);
+
 vc_map(array(
-    "name" => "Slider Banner",
-    "base" => "bestel_slick_slider_banner",
-    "icon" => BESTEL_THEME_URI . '/images/best-hotel-icon.png',
-    "category" => esc_html__('Bestel', BESTEL_HOTEL_CORE),
+    "name" => "Banner",
+    "base" => "themename_banner",
+    "icon" => THEME_URI . '/images/best-hotel-icon.png',
+    "category" => esc_html__('themename', 'text-domain'),
     "content_element" => true,
     "show_settings_on_create" => true,
     "params" => array(
@@ -19,60 +16,46 @@ vc_map(array(
 		"param_name" => "heading_1",
 	),
 	
-	$fonts_array[0],
-    $fonts_array[1],
-    $fonts_array[2],
-    $fonts_array[3],
-    $fonts_array[4],
-    $fonts_array[5],
-    $fonts_array[6],
-    $fonts_array[7],
 	
 	array(
 		 'type' => 'param_group',
 		  'value' => '',
-		  'heading' =>  __( 'List Items', 'pt-vc' ),
+		  'heading' =>  __( 'List Items', 'text-domain' ),
 		  'param_name' => 'heading1_list',
 		  'params' => array(
 			  array(
 				 'type' => 'textfield',
 				 'value' => '',
-				 'heading' => __( 'Title', 'pt-vc' ),
+				 'heading' => __( 'Title', 'text-domain' ),
 				 'param_name' => 'heading1_title',
 			 ),
 			 array(
 				'type' => 'iconpicker',
-				'heading' => __( 'Icon', 'js_composer' ),
+				'heading' => __( 'Icon', 'text-domain' ),
 				'param_name' => 'heading1_bestelicons',
 					'settings' => array(
 					'emptyIcon' => false, // default true, display an "EMPTY" icon?
 					'type' => 'bestelicons',
 					'iconsPerPage' => 200, // default 100, how many icons per/page to display
 					),
-				'description' => __( 'Select icon from library.', 'js_composer' ),
+				'description' => __( 'Select icon from library.', 'text-domain' ),
 				),
 			array(
 				 'type' => 'textfield',
 				 'value' => '',
-				 'heading' => __( 'Icon Class', 'pt-vc' ),
+				 'heading' => __( 'Icon Class', 'text-domain' ),
 				 'param_name' => 'heading1_icon',
 			 ),
 			 array(
                 "type" => "vc_link",
                 "holder" => "div",
-                "heading" => __( 'Link', 'pt-vc' ),
+                "heading" => __( 'Link', 'text-domain' ),
                 "param_name" => "heading1_call_action",
              ),
 			 
 		
 )
 ),
-	
-	
-	
-	
-	
-	
 	
 	
 	array(
@@ -83,44 +66,35 @@ vc_map(array(
 		"param_name" => "heading_2",
 	),
 	
-	$fonts_array2[0],
-    $fonts_array2[1],
-    $fonts_array2[2],
-    $fonts_array2[3],
-    $fonts_array2[4],
-    $fonts_array2[5],
-    $fonts_array2[6],
-    $fonts_array2[7],
-	
 	
 	array(
 		 'type' => 'param_group',
 		  'value' => '',
-		  'heading' =>  __( 'List Items', 'pt-vc' ),
+		  'heading' =>  __( 'List Items', 'text-domain' ),
 		  'param_name' => 'heading2_list',
 		  'params' => array(
 			  array(
 				 'type' => 'textfield',
 				 'value' => '',
-				 'heading' => __( 'Title', 'pt-vc' ),
+				 'heading' => __( 'Title', 'text-domain' ),
 				 'param_name' => 'heading2_title',
 			 ),
 			 array(
 				 'type' => 'textfield',
 				 'value' => '',
-				 'heading' => __( 'Sub Title', 'pt-vc' ),
+				 'heading' => __( 'Sub Title', 'text-domain' ),
 				 'param_name' => 'heading2_title1',
 			 ),
 			array(
 				 'type' => 'attach_image',
 				 'value' => '',
-				 'heading' => __( 'Image', 'pt-vc' ),
+				 'heading' => __( 'Image', 'text-domain' ),
 				 'param_name' => 'heading2_img',
 			 ),
 			 array(
                 "type" => "vc_link",
                 "holder" => "div",
-                "heading" => __( 'Link', 'pt-vc' ),
+                "heading" => __( 'Link', 'text-domain' ),
                 "param_name" => "heading2_call_action",
              ),
 		  )
@@ -137,16 +111,6 @@ vc_map(array(
 		"param_name" => "heading_3",
 	),
 	
-	$fonts_array3[0],
-    $fonts_array3[1],
-    $fonts_array3[2],
-    $fonts_array3[3],
-    $fonts_array3[4],
-    $fonts_array3[5],
-    $fonts_array3[6],
-    $fonts_array3[7],
-
-        
 	
 	array(
 		 'type' => 'param_group',
@@ -199,7 +163,7 @@ vc_map(array(
 
 if (class_exists('WPBakeryShortCode')) {
 
-    class WPBakeryShortCode_Bestel_Slick_Slider_Banner extends WPBakeryShortCode {
+    class WPBakeryShortCode_themename_Banner extends WPBakeryShortCode {
 
     }
 
